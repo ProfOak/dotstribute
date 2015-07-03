02174874db6a2524c78942095607f4e94e61daaf
ProfOak <openprofoak@gmail.com>
2015-07-03 13:50:36 -0500

    changed excludes to ignores

--- dotstribute.py
+++ dotstribute.py
@@ -3,24 +3,24 @@ import os
 from optparse import OptionParser
 
 class Dot():
-    def __init__(self, gd):
-        self.git_dir = gd
+    def __init__(self, git_dir):
+        self.git_dir = git_dir
 
-    def exclude(self, dotexclude=".dotexclude"):
-        # use the .dotexclude file IN the git directory, not in cwd
-        exclude_file = dotexclude
+    def get_files(self, dotignore=".dotignore"):
+        # use the .dotignore file IN the git directory, not in cwd
+        ignore_file = dotignore
         if self.git_dir != ".":
-            exclude_file = self.git_dir + dotexclude
+            ignore__file = self.git_dir + dotignore
 
         # prepare list of files not to link to $HOME
-        EXCLUDE = []
-        if os.path.exists(exclude_file):
-            with open(exclude_file) as ef:
-                EXCLUDE = ef.read().split()
-        # still never add the .dotexclude file to $HOME
-        EXCLUDE.append(dotexclude)
+        IGNORE = []
+        if os.path.exists(ignore_file):
+            with open(ignore_file) as ef:
+                IGNORE = ef.read().split()
+        # still never add the .dotignore file to $HOME
+        IGNORE.append(dotignore)
 
-        self.files = [f for f in os.listdir(self.git_dir) if f not in EXCLUDE]
+        self.files = [f for f in os.listdir(self.git_dir) if f not in IGNORE]
 
     def link(self):
         # by default place dotfiles in your $HOME
@@ -44,8 +44,8 @@ class Dot():
 
 def main():
     parser = OptionParser()
-    parser.add_option("-d", "--dotexe", dest = "dot_exclude",
-            help = "Exclude files, given by .dotexclude file")
+    parser.add_option("-d", "--dotignore", dest = "dot_ignore",
+            help = "Exclude files, given by .dotignore file")
     parser.add_option("-f", "--force", dest = "force", default = False,
             action = "store_true", help = "Force overwrite the previous links")
     parser.add_option("-u", "--unlink", dest = "unlink", default = False,
@@ -53,8 +53,8 @@ def main():
 
     (options, args) = parser.parse_args()
 
-    if options.dot_exclude:
-        dotexclude = options.dot_exclude
+    if options.dot_ignore:
+        dotignore = options.dot_ignore
 
     git_dir = "."
     if len(args) == 1:
@@ -68,7 +68,7 @@ def main():
                 return
 
     d = Dot(git_dir)
-    d.exclude(".dotexclude")
+    d.get_files(".dotignore")
     d.link()
 
 
