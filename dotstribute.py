bbc0ba80a6c73cc2810b3ca4099f8156991ce984
ProfOak <openprofoak@gmail.com>
2015-07-03 13:34:53 -0500

    code starting to get large, reformat

--- dotstribute.py
+++ dotstribute.py
@@ -2,21 +2,60 @@
 import os
 from optparse import OptionParser
 
+class Dot():
+    def __init__(self, gd):
+        self.git_dir = gd
+
+    def exclude(self, dotexclude=".dotexclude"):
+        # use the .dotexclude file IN the git directory, not in cwd
+        exclude_file = dotexclude
+        if self.git_dir != ".":
+            exclude_file = self.git_dir + dotexclude
+
+        # prepare list of files not to link to $HOME
+        EXCLUDE = []
+        if os.path.exists(exclude_file):
+            with open(exclude_file) as ef:
+                EXCLUDE = ef.read().split()
+        # still never add the .dotexclude file to $HOME
+        EXCLUDE.append(dotexclude)
+
+        self.files = [f for f in os.listdir(self.git_dir) if f not in EXCLUDE]
+
+    def link(self):
+        # by default place dotfiles in your $HOME
+        for f in self.files:
+            to = os.environ["HOME"] + "/"
+            if not f.startswith("."):
+                to += "."
+            to += f
+
+            # for correct symlinking
+            f = os.getcwd() + "/" + self.git_dir + f
+
+            # add option: replace (ask)
+            # add option: force replace (no ask)
+            if not os.path.exists(to):
+                os.symlink(f, to)
+            else:
+                print "skipping", f
+
+            # add option: chmod symlink
+
 def main():
     parser = OptionParser()
     parser.add_option("-d", "--dotexe", dest = "dot_exclude",
             help = "Exclude files, given by .dotexclude file")
     parser.add_option("-f", "--force", dest = "force", default = False,
             action = "store_true", help = "Force overwrite the previous links")
+    parser.add_option("-u", "--unlink", dest = "unlink", default = False,
+            action = "store_true", help = "Remove the previous links")
 
     (options, args) = parser.parse_args()
 
-    # option for custom dotexclude file
-    dotexclude = ".dotexclude"
     if options.dot_exclude:
         dotexclude = options.dot_exclude
 
-    # get files not in exclude list
     git_dir = "."
     if len(args) == 1:
         if os.path.exists(args[0]):
@@ -28,39 +67,10 @@ def main():
                 print "Now exiting"
                 return
 
-    # use the .dotexclude file IN the git directory, not in cwd
-    exclude_file = dotexclude
-    if git_dir != ".":
-        exclude_file = git_dir + dotexclude
+    d = Dot(git_dir)
+    d.exclude(".dotexclude")
+    d.link()
 
-    # prepare list of files not to link to $HOME
-    EXCLUDE = []
-    if os.path.exists(exclude_file):
-        with open(git_dir + dotexclude) as f:
-            EXCLUDE = f.read().split()
-    # still never add the .dotexclude file to $HOME
-    EXCLUDE.append(dotexclude)
-
-    files = [f for f in os.listdir(git_dir) if f not in EXCLUDE]
-
-    # by default place dotfiles in your $HOME
-    for f in files:
-        to = os.environ["HOME"] + "/"
-        if not f.startswith("."):
-            to += "."
-        to += f
-
-        # for correct symlinking
-        f = os.getcwd() + "/" + git_dir + f
-
-        # add option: replace (ask)
-        # add option: force replace (no ask)
-        if not os.path.exists(to):
-            os.symlink(f, to)
-        else:
-            print "skipping", f
-
-        # add option: chmod symlink
 
 if __name__ == "__main__":
     main()
