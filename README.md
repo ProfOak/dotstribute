147754731657118edb6b02889430b90725d9e06e
ProfOak <585087+ProfOak@users.noreply.github.com>
2024-01-26 19:55:22 -0600

    Working version with readme

---
+++ README.md
@@ -0,0 +1,44 @@
+Dotstribute: A dotfile manager tool
+---
+
+Dotstribute will recursively place symlinks of files in your current working
+directory to your home directory. Only the files themselves will be symlinked.
+Any sort of directory structure will be recreated as part of a run, but the
+directories will not be symlinked.
+
+The dotfiles stored in current working directory do not need to start with a
+dot. Dotstribute will add the dot to the symlink when running. This lets you
+store them as non-hidden files in a VCS folder. This only applies to the top
+level of files and directories. Anything further down the directory tree that
+require files start with a dot will need to be stored with dots.
+
+Dotstribute will let you undo the symlinks after they're placed. Run
+`dotstribute` in your dotfile directory with the `--unlink` flag to remove them
+from your home directory. This currently does not delete the directories
+themselves if they only contain the user's dotfiles but I could see that being
+a useful feature in the future.
+
+Dotstribute is aware you might not want to symlink everything in a directory. A
+`.git` file probably doesn't need to be symlinked to a home directory. You can
+add exclusions to a special filename called `.dotignore`. This file should
+contain a list of file or directory names you do not want symlink separated by
+a new line.
+
+Installation:
+
+```
+go install
+```
+
+Usage:
+
+```
+> dotstribute --help
+Usage of dotstribute:
+  -a, --ask
+  -g, --generate
+  -p, --preview
+  -u, --unlink
+```
+
+
---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	flag "github.com/spf13/pflag"
+)
+
+func getIgnoredFiles(dotignoreFile string) []string {
+	ignoredFiles := []string{}
+	buff, err := os.ReadFile(dotignoreFile)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			log.Println("Unable to find the .dotstribute file. Continuing without it.")
+		} else if errors.Is(err, os.ErrPermission) {
+			log.Println(".dotstribute file permission error. Continuing without it.")
+		} else {
+			log.Fatalln(err)
+		}
+	}
+
+	for _, ignoredFile := range strings.Split(string(buff), "\n") {
+		if ignoredFile != "" {
+			ignoredFiles = append(ignoredFiles, ignoredFile)
+		}
+	}
+	return ignoredFiles
+}
+
+func shouldIgnore(path string, ignoredFiles []string) bool {
+	for _, ignoredFile := range ignoredFiles {
+		if strings.HasPrefix(path, ignoredFile) {
+			return true
+		}
+	}
+	return false
+}
+
+func getDotFiles(starting string, ignoredFiles []string) []string {
+	var dotfiles []string
+	err := filepath.WalkDir(starting, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if shouldIgnore(info.Name(), ignoredFiles) {
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+		} else if !info.IsDir() {
+			// Only save leaves of the filepath tree.
+			dotfiles = append(dotfiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// First one is `.` and we don't want to skip it because filepath.SkipDir
+	// will exclude the whole path under the current working directory.
+	return dotfiles[1:]
+}
+
+func homePath(dotfile string) string {
+	// Files might not have the necessary dot prefix in the dotfile repo.
+	if !strings.HasPrefix(dotfile, ".") {
+		dotfile = "." + dotfile
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln("Unable to find the home directory.")
+	}
+	return filepath.Join(home, dotfile)
+}
+
+func symlink(dotfile string, preview bool, ask bool) {
+	symlinkPath := homePath(dotfile)
+	dotfile, err := filepath.Abs(dotfile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s -> %s\n", dotfile, symlinkPath)
+	if preview {
+		return
+	}
+	if _, err := os.Stat(symlinkPath); !errors.Is(err, os.ErrNotExist) {
+		fmt.Println("File already exists")
+		return
+	}
+
+	if !ask || shouldContinue() {
+		dir, _ := filepath.Split(symlinkPath)
+		err = os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			log.Println("Unable to create the directory needed.")
+			log.Fatalln(err)
+		}
+		err = os.Symlink(dotfile, symlinkPath)
+		if err != nil {
+			log.Println("Unable to symlink the file needed.")
+			log.Fatalln(err)
+		}
+	}
+}
+
+func unsymlink(dotfile string, preview bool, ask bool) {
+	symlinkPath := homePath(dotfile)
+	dotfile, err := filepath.Abs(dotfile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if _, err := os.Stat(symlinkPath); os.IsNotExist(err) {
+		log.Printf("Skipping: No symlink '%s' in home directory.", symlinkPath)
+		return
+	}
+	fmt.Printf("Removing: %s\n", symlinkPath)
+	if preview {
+		return
+	}
+
+	if !ask || shouldContinue() {
+		err = os.Remove(symlinkPath)
+		if err != nil {
+			log.Println("Unable to remove the symlink")
+			log.Fatalln(err)
+		}
+	}
+}
+
+func shouldContinue() bool {
+	var choice string
+	fmt.Print("Continue? (y/N): ")
+	fmt.Scanln(&choice)
+	return strings.ToLower(choice) == "y"
+}
+
+func main() {
+	var (
+		generate bool
+		preview  bool
+		unlink   bool
+		ask      bool
+	)
+	flag.BoolVarP(&generate, "generate", "g", false, "")
+	flag.BoolVarP(&preview, "preview", "p", false, "")
+	flag.BoolVarP(&unlink, "unlink", "u", false, "")
+	flag.BoolVarP(&ask, "ask", "a", false, "")
+	flag.Parse()
+
+	action := symlink
+	if unlink {
+		action = unsymlink
+	}
+
+	if generate {
+		return
+	}
+
+	ignoredFiles := getIgnoredFiles(".dotignore")
+	dotfiles := getDotFiles(".", ignoredFiles)
+	for _, f := range dotfiles {
+		action(f, preview, ask)
+	}
+}
